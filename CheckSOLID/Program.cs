57f9b96b31789da5a7388b52b89cf165df623892
courier66666 <petzik.popovich@gmail.com>
2023-12-21 17:38:50 +0200

    new commit

---
+++ CheckSOLID/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SOLIDCheckingLibrary;
+
+
+Console.WriteLine("Hello, World!");
+OpenExtClosedMod.IsClassExtensible(typeof(A), ignoreInheritedClasses: false);
+
+D c = new A();
+
+c.B_(1);
+
+class C
+{
+    public virtual void B_(int a)
+    {
+        Console.WriteLine("B");
+    }
+}
+interface IB
+{
+    void B_interface();
+}
+abstract class D : C, IA, IB
+{
+    private int d_var = 1;
+    public int Biba { get; set; }
+    public virtual int Ciba { get; set; }
+    public abstract int Diba {get;set;} 
+    public void A_interface()
+    {
+        Console.WriteLine("D");
+    }
+
+    public override void B_(int a)
+    {
+        Console.WriteLine("B");
+    }
+
+    public void B_interface()
+    {
+        throw new NotImplementedException();
+    }
+}
+interface IA
+{
+    void A_interface();
+    int Biba { get; set; }
+}
+class A : D
+{
+    private int i;
+    private List<int> list;
+    public void A_interface()
+    {
+        Console.WriteLine("A");
+    }
+    public A(IDictionary<List<int>,int> dict)
+    {
+
+    }
+    public A()
+    {
+
+    }
+    public A(int i)
+    {
+        this.i = i;
+    }
+    public int Sum(A a, int b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Ha
+    {
+        get { throw new NotImplementedException(); }
+        set { throw new NotImplementedException(); }
+
+    }
+
+    public override int Diba { get; set; }
+}
+
+
+
+
---
+++ CheckSOLID/SOLIDCheckingLibrary/InterfaceSegregation/InterfaceSegregation.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDCheckingLibrary
+{
+    public static class InterfaceSegregation
+    {
+        private static object? CreateInstanceOfClass(Type classType)
+        {
+            var firstConstructor = classType.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).First();
+            var arguments = firstConstructor.GetParameters().
+                Select(p => p.ParameterType.IsValueType ? Activator.CreateInstance(p.ParameterType) : null).
+                ToArray();
+            
+            return firstConstructor.Invoke(arguments);
+        }
+        
+        public static bool MethodIsImplemented(MethodInfo methodInfo, Type initialClass)
+        {
+            var arguments = methodInfo.GetParameters().
+                Select(p => p.ParameterType.IsValueType ? Activator.CreateInstance(p.ParameterType) : null).
+                ToArray();
+
+            try
+            {
+                var instance = CreateInstanceOfClass(initialClass);
+                methodInfo.Invoke(instance, arguments);
+            }
+            catch(TargetInvocationException ex)
+            {
+                if(ex.InnerException is NotImplementedException)
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                return true;
+            }
+
+            return true;
+        }
+        public static bool PropertyGetIsImplemented(PropertyInfo propertyInfo, Type initialClass)
+        {
+            var getMethod = propertyInfo.GetGetMethod();
+            if(getMethod == null)
+                return false;
+
+            try
+            {
+                var instance = CreateInstanceOfClass(initialClass);
+                propertyInfo.GetValue(instance);
+            }
+            catch (TargetInvocationException ex)
+            {
+                if (ex.InnerException is NotImplementedException)
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                return true;
+            }
+
+            return true;
+        }
+        public static bool PropertySetIsImplemented(PropertyInfo propertyInfo, Type initialClass)
+        {
+            var getMethod = propertyInfo.GetSetMethod();
+            if (getMethod == null)
+                return false;
+
+            try
+            {
+                var setArgument = propertyInfo.PropertyType.IsValueType ? Activator.CreateInstance(propertyInfo.PropertyType) : null;
+                var instance = CreateInstanceOfClass(initialClass);
+                propertyInfo.SetValue(instance,setArgument);
+            }
+            catch (TargetInvocationException ex)
+            {
+                if (ex.InnerException is NotImplementedException)
+                    return false;
+            }
+            catch(Exception ex)
+            {
+                return true;
+            }
+
+            return true;
+        }
+        public static (bool, string) ClassMethodsFollowPrinciple(Type type)
+        {
+            var allMethods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            bool followsPrinciple = true;
+            string checkLog = "";
+
+            foreach (var method in allMethods)
+            {
+                if(!MethodIsImplemented(method, type))
+                {
+                    followsPrinciple = false;
+                    checkLog += $"Method {method.ToString()} is not implemented!\n";
+                }
+            }
+            if (followsPrinciple) checkLog = $"Methods of class {type.Name} follow the principle";
+            else checkLog = $"Methods of class {type.Name} failed to follow the principle!\n" + checkLog;
+
+            return (followsPrinciple, checkLog);
+        }
+        public static (bool, string) ClassPropertiesFollowPrinciple(Type type)
+        {
+            var allProperties = type.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            bool followsPrinciple = true;
+            string checkLog = "";
+
+            foreach(var property in allProperties)
+            {
+                bool propertyIsImplemented = true;
+                string checkPropertyLog = "";
+                if(property.GetGetMethod() != null)
+                {
+                    if(!PropertyGetIsImplemented(property, type))
+                    {
+                        propertyIsImplemented = false;
+                        checkPropertyLog += "Getter is not implemented!\n";
+                    }
+                }
+                if(property.GetSetMethod() != null)
+                {
+                    if (!PropertySetIsImplemented(property, type))
+                    {
+                        propertyIsImplemented = false;
+                        checkPropertyLog += "Setter is not implemented!\n";
+                    }
+                }
+
+                if (!propertyIsImplemented) 
+                    checkLog += $"Property {property.ToString()} is not implemented!\n" + checkPropertyLog;
+
+                followsPrinciple &= propertyIsImplemented;
+            }
+
+            if (followsPrinciple) checkLog = $"Properties of class {type.Name} follow the principle";
+            else checkLog = $"Properites of class {type.Name} failed to follow the principle!\n" + checkLog;
+
+            return (followsPrinciple, checkLog);
+        }
+        public static (bool, string) ClassFollowsPrinciple(Type type)
+        {
+            bool followsPrinciple = true;
+            string checkLog = "";
+            var methodsCheck = ClassMethodsFollowPrinciple(type);
+            var propertiesCheck = ClassPropertiesFollowPrinciple(type);
+            if(!methodsCheck.Item1)
+            {
+                checkLog += methodsCheck.Item2 + "\n";
+                followsPrinciple = false;
+            }
+            if(!propertiesCheck.Item1)
+            {
+                checkLog += propertiesCheck.Item2 + "\n";
+                followsPrinciple = false;
+            }
+            if (followsPrinciple) checkLog = $"Class {type.Name} follows the principle of interface segregation";
+            else checkLog = $"Class {type.Name} doesn't follows the principle of interface segregation:\n" + checkLog;
+            return (followsPrinciple, checkLog);
+        }
+    }
+}
